99aadddcec325f8d048f2ed90a1551c41e759332
Bruce-Mahagwa <jacobbruce880@gmail.com>
2024-08-07 21:28:13 +0300

    Removes unnecessary styling

--- portfolio_v1/app/page.js
+++ portfolio_v1/app/page.js
@@ -6,17 +6,7 @@ export default function Home() {
     <main className={styles.main}>
      
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
+    
       <div className={styles.grid}>
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
