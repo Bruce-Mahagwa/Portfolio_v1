d65b5f6c88a26c265e82e7d47c22e06891e32d29
Bruce-Mahagwa <jacobbruce880@gmail.com>
2024-08-29 16:21:02 +0300

    Removes unnecessary files

--- portfolio_v1/app/page.js
+++ portfolio_v1/app/page.js
@@ -2,12 +2,14 @@
 import Landing  from "@/components/Landing/Landing";
 import About from "@/components/AboutMe/About";
 import "./globals.css"
+import Projects from "@/components/Projects/Projects";
 
 export default function Home() {
   return (
     <>
       <Landing />
       <About />
+      <Projects />
     </>
   );
 }
---
+++ portfolio_v1/components/Projects/Projects.js
@@ -0,0 +1,9 @@
+// files
+import classes from "./Projects.module.css"
+
+const Projects = () => {
+    return (
+        "Projects"
+    )
+}
+export default Projects;
